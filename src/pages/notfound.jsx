d58209d461f8a0da41c45c1331404df71d8c9525
actualchrisw <actualchrisw@gmail.com>
2025-11-30 15:46:31 -0700

    account and not found updated

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -4,8 +4,8 @@ export default function NotFound() {
   return (
     <section className="page not-found">
       <h1>404</h1>
-      <p>Sorry, we couldn&apos;t find that page.</p>
-      <Link className="btn primary" to="/">
+      <p>Sorry, we couldn't find that page.</p>
+      <Link to="/">
         Back to Home
       </Link>
     </section>
