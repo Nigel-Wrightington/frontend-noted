9f13b17c292a015f68fdbcffde3b4c5ca3acfe5b
Sherin <sherin.daniel89@yahoo.com>
2025-12-01 21:20:47 -0500

    Navbar - (placeholder to be removed later)

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 
+// These are just temp placeholder for Navbar to work //=====
+function Browse() {
+  return <p>Browse page (placeholder)</p>;
+}
+function Account() {
+  return <p>Account page (placeholder)</p>;
+}
+
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<p>Home page</p>} />
         <Route path="/register" element={<Register />} />
+        {/* Required for Navbar slice */}
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/account" element={<Account />} />
         <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,19 +1,37 @@
-import { NavLink } from "react-router";
-
+import { NavLink, useNavigate } from "react-router";
 import { useAuth } from "../auth/AuthContext";
 
 export default function Navbar() {
-  const { token, logout } = useAuth();
+  const { isAuthenticated, logout } = useAuth();
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    logout(); // Clears token + user in AuthContext
+    navigate("/"); // Send user back to Home after logout
+  }
   return (
     <header id="navbar">
+      {/* Website name on the left // Optional, incase we need it for css */}
       <NavLink id="brand" to="/">
-        <p>Frontend Template</p>
+        <p>Noted</p>
       </NavLink>
+
+      {/* Navigation links */}
       <nav>
-        {token ? (
-          <button onClick={logout}>Log out</button>
+        {/* Always visible on all states (logged in/out) */}
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/browse">Browse</NavLink>
+        <NavLink to="/account">Account</NavLink>
+
+        {isAuthenticated ? (
+          // Logged IN: show only Logout // will shwo all other links too //
+          <button onClick={handleLogout}>Logout</button>
         ) : (
-          <NavLink to="/login">Log in</NavLink>
+          // Logged OUT: show Login + Register plus all other navlinks //
+          <>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/register">Register</NavLink>
+          </>
         )}
       </nav>
     </header>
